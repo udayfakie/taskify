03c9ee8458bc9f4283c390db53d7885403c783a1
udayfakie <92738263+udayfakie@users.noreply.github.com>
2023-09-22 23:41:46 +0300

    appfxied

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import StoredData from "./StoredData";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./navbar/NavBar";
-import Panda from "./components/screens/Panda";
-import Home from "./components/screens/Home";
-import { Todo } from "./model";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import NavBar from "./navbar/NavBar";
+// import Panda from "./components/screens/Panda";
+// import Home from "./components/screens/Home";
+// import { Todo } from "./model";
 
 const App: React.FC = () => {
-  const todos: Todo[] = [{ id: 1, todo: "Buy groceries", isDone: false }];
+  // const todos: Todo[] = [{ id: 1, todo: "Buy groceries", isDone: false }];
   return (
-    <Router>
+    <>
       <StoredData/>
       {/* <NavBar />  */}
       {/* <Routes> */}
@@ -17,7 +17,7 @@ const App: React.FC = () => {
         <Route path="/todo" element={<StoredData />} />
         <Route path="/panda" element={<Panda />} /> 
       </Routes> */}
-    </Router>
+    </>
   );
 };
 
